acfaebab87d174a08e452a073d62d6f74f719e93
bn-erisson <erissonnunes@hotmail.com>
2019-03-20 17:27:23 -0300

    Modificaçoes

--- Atividade1/Conta.js
+++ Atividade1/Conta.js
@@ -1,25 +1,74 @@
 "use strict";
+//Erisson Bezerra Nunes
+//402876
+// Ps: Ta BEEEM imcompleto e cheio de erros :/
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 exports.__esModule = true;
-var conta = /** @class */ (function () {
-    function conta(saldo, numeroDeConta) {
+var Conta = /** @class */ (function () {
+    function Conta(saldo, numeroDeConta) {
         this.saldo = saldo;
         this.numeroDeConta = numeroDeConta;
     }
-    conta.prototype.getRelatorio = function (tipoDeConta, numeroDeConta, saldo, rendimento) {
-        var relatorio;
-        relatorio = "Seu relatorio: " + "\n" + "Tipo de conta : " + tipoDeConta + "\n" +
-            "Numero de conta: " + numeroDeConta + "\n" +
-            "Saldo de conta: " + saldo + "\n" +
-            "Rendimento: " + rendimento;
-        return relatorio;
+    Conta.prototype.depositar = function (quantia) {
+        this.saldo += quantia;
     };
-    conta.prototype.rendimento = function (saldo) {
+    Conta.prototype.saquar = function (quantia) {
+        if (quantia > this.saldo) {
+            console.log("Saldo insuficiente");
+            return false;
+        }
+        else {
+            this.saldo -= quantia;
+            return true;
+        }
     };
-    conta.prototype.deposito = function (valor) {
-        saldo: this.saldo + valor;
-    };
-    return conta;
+    return Conta;
 }());
-exports.conta = conta;
-//let conta1 = new conta (2.99, "223344") 
-console.log(conta1.getRelatorio("contax", "223344", 2.98, 3));
+exports.Conta = Conta;
+var ContaCorrente = /** @class */ (function (_super) {
+    __extends(ContaCorrente, _super);
+    function ContaCorrente() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    ContaCorrente.prototype.saquar = function (quantia) {
+        quantia = quantia + 2;
+        if (quantia > this.saldo) {
+            console.log("Saldo insuficiente");
+            return false;
+        }
+        else {
+            this.saldo -= quantia;
+            return true;
+        }
+    };
+    return ContaCorrente;
+}(Conta));
+;
+var ContaPoupança = /** @class */ (function (_super) {
+    __extends(ContaPoupança, _super);
+    function ContaPoupança() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    return ContaPoupança;
+}(Conta));
+;
+var ContaInvestimento = /** @class */ (function (_super) {
+    __extends(ContaInvestimento, _super);
+    function ContaInvestimento() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    return ContaInvestimento;
+}(Conta));
+;
--- Atividade1/Conta.ts
+++ Atividade1/Conta.ts
@@ -3,98 +3,79 @@
 // Ps: Ta BEEEM imcompleto e cheio de erros :/
 
 
-export abstract class conta{
+abstract class Conta{
     public tipoDeConta : string;
     public saldo: number;
     public numeroDeConta: string;
+    public taxaSelic : number = 0.08;
 
     public constructor(saldo: number, numeroDeConta: string){
         this.saldo = saldo;
         this.numeroDeConta = numeroDeConta;
     }
 
-    public abstract getRelatorio(): { // não é pra isso estar aqui
-       
+    public depositar(quantia:number){
+        this.saldo += quantia;
     }
 
-    public abstract imposto(): {
+    public sacar(quantia:number):boolean {
+        if (quantia > this.saldo){
+            console.log("Saldo insuficiente")
+            return false
+        }
+        else {
+            this.saldo -= quantia;
+            return true
+        }
     }
+    // metodo abstrato
+    abstract valorisar();
 
-    public abstract deposito(): {
-
-    }
-
-    public abstract saque(): {
-
-    }
-
-    public abstract rentimento(): {
-
-    }
-    
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-class contaCorrente extends conta{
-
-    public tipoDeConta : string;
-    public saldo: number;
-    public numeroDeConta: string;
-
-    public constructor (saldo: number, numeroDeConta: string){
-        super(saldo,numeroDeConta);
-
-    }
-
-    
-    public getRelatorio(tipoDeConta: string, numeroDeConta:string, saldo: number, rendimento:number ): string{
-        let relatorio : string;
-        relatorio = "Seu relatorio: " + "\n" + "Tipo de conta : " + tipoDeConta + "\n" + 
-        "Numero de conta: " + numeroDeConta + "\n" +
-        "Saldo de conta: " + saldo + "\n" +
-        "Rendimento: " + rendimento;
-
-        return relatorio;
-    }
-
-    public imposto(saque:number):number{
-        
-        let imposto = saque - 2;
-        return imposto;
-    }
-
-    public deposito(valor:number):string{
-        this.saldo = this.saldo = valor;
-
-    }
-
-    public saque(valor:number):string{
-        let saque : number = valor - imposto();
-
-    }
-
-    public rendimento():number{
-        
-    } 
-
-
-} 
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-class contaPoupança extends conta{
-
-    public tipoDeConta : string;
-    public saldo: number;
-    public numeroDeConta: string;
-
-    public constructor (saldo: number, numeroDeConta: string){
-        super(saldo,numeroDeConta);
-
-    }
-    
-    public 
-
-
+interface IcontaRende {
+    gerarRelatorio()
 }
+
+class ContaCorrente extends Conta{
+    public sacar(quantia:number) : boolean {
+        let quantiaTx : number = quantia + 2;
+        return super.sacar(quantiaTx);
+    }
+
+    valorisar(){
+        console.log("não valorisa");
+    }
+};
+
+class ContaPoupança extends Conta implements IcontaRende{
+    public sacar(quantia:number) : boolean{
+        let quantiaTx : number = quantia + 0.02 * quantia;
+        return super.sacar(quantiaTx);
+    }
+
+    valorisar(){
+        this.saldo += this.saldo * 0.02;
+    }
+
+    gerarRelatorio(){
+        console.log("INVESTIMENTO");
+        console.log("Numero: " + this.numeroDeConta);
+        console.log("Saldo: " + this.saldo);
+        console.log("redimento: " + this.saldo*1+this.taxaSelic);
+    }
+};
+
+class ContaInvestimento extends Conta{
+
+    public sacar(quantia : number) : boolean{
+        let quantiaTx : number = quantia + 0.05 * quantia + 10
+        return super.sacar(quantia);
+    }
+
+    valorisar(){
+        this.saldo += this.saldo * this.taxaSelic;
+    }
+};
+
+let cc : Conta = new ContaCorrente(100.00, "402876")
